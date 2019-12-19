9c3b5e9016090f3a91a5763877328f18c278e198
Girmay <ink423103@yahoo.com>
2019-12-19 15:05:49 +0100

    print number added

---
+++ PrintNumber/PrintNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrintNumber
+{
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the numbers from 1 to 100, but for multiples of 3 print Foo, for multiples of 5 print Bar and for numbers that are multiples of both 3 and 5 print FooBar. ");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FooBar");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Foo");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Bar");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }           
+   
+        }
+    }
+}
